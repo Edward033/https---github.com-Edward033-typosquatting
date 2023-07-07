dc126e191711e9de2afb723ffe1e5b364bb1a6b6
Edward Alvarez <edwardalvarezm@gmail.com>
2023-07-07 15:00:18 -0400

    Adding arguments to application.py

--- application.py
+++ application.py
@@ -1,4 +1,17 @@
 from typosquatting import TypoSquatting
+import argparse
+
+argument_parser = argparse.ArgumentParser(
+    prog='Typosquatting.py',
+    description=('Print candidate domains for typosquatting based on edit' 
+                 ' distance calculation. Reads from a csv whois database file'
+                 ' with newly registered domain names')
+)
+argument_parser.add_argument("-d", "--domain")
+argument_parser.add_argument("-e", "--edit_distance")
+argument_parser.add_argument("-w", "--whois_file", required=False)
+
+arguments = argument_parser.parse_args() 
 
 typosquatting = TypoSquatting(original_domain = arguments.domain, 
     edit_distance=arguments.edit_distance,
